8cf21751e9672773c4c671424dceef3defd565a8
Tanjina <tanjinaproma@gmail.com>
2024-05-11 22:45:46 +0600

    start_data_ingestion method added

--- ism/pipeline/training_pipeline.py
+++ ism/pipeline/training_pipeline.py
@@ -9,3 +9,35 @@ from ism.entity.config_entity import (DataIngestionConfig)
 
 from ism.entity.artifacts_entity import (DataIngestionArtifact)
 
+
+
+class TrainPipeline:
+    def __init__(self):
+        self.data_ingestion_config = DataIngestionConfig()
+
+
+
+    def start_data_ingestion(self)-> DataIngestionArtifact:
+        try: 
+            logging.info(
+                "Entered the start_data_ingestion method of TrainPipeline class"
+            )
+            logging.info("Getting the data from URL")
+
+            data_ingestion = DataIngestion(
+                data_ingestion_config =  self.data_ingestion_config
+            )
+
+            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+            logging.info("Got the data from URL")
+            logging.info(
+                "Exited the start_data_ingestion method of TrainPipeline class"
+            )
+
+            return data_ingestion_artifact
+
+        except Exception as e:
+            raise ismException(e, sys)
+        
+
+    
