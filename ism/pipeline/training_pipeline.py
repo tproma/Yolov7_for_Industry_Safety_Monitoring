b10f4ff5ca61147c2d37edd42a23deb5160d22dc
Tanjina <tanjinaproma@gmail.com>
2024-05-11 22:37:13 +0600

    training pipeline modified

--- ism/pipeline/training_pipeline.py
+++ ism/pipeline/training_pipeline.py
@@ -4,3 +4,8 @@ from ism.exception import ismException
 from ism.configuration.s3_operations import S3Operation
 from ism.components.data_ingestion import DataIngestion
 
+
+from ism.entity.config_entity import (DataIngestionConfig)
+
+from ism.entity.artifacts_entity import (DataIngestionArtifact)
+
